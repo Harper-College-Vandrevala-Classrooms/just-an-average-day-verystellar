b5aec25f2c8db6b77d894184858ff9a4fbbb2374
verystellar <161178496+verystellar@users.noreply.github.com>
2024-12-13 20:15:09 -0600

    implemented calculator with by both for loop and stream methods

--- src/main/java/com/csc/Calculator.java
+++ src/main/java/com/csc/Calculator.java
@@ -1,6 +1,60 @@
 package com.csc;
 
+import java.util.Arrays;
+
 public class Calculator {
+  public int maximumUsingForLoop(int[] nums) {
+    if (nums == null || nums.length == 0) { return 0; }
+    int max = nums[0];
+    for (int i = 1; i < nums.length; i++) {
+      if (max < nums[i]) {
+        max = nums[i];
+      }
+    }
+    return max;
+  }
+  public int minimumUsingForLoop(int[] nums) {
+  if (nums == null || nums.length == 0) { return 0; }
+  int min = nums[0];
+  for (int i = 1; i < nums.length; i++) {
+    if (min > nums[i]) {
+      min = nums[i]; }
+  }
+  return min;
+  }
+  public int sumUsingForLoop(int[] nums) {
+  if (nums == null || nums.length == 0) { return 0; }
+  int sum = 0;
+      for (int num : nums) {
+          sum += num;
+      }
+  return sum;
+  }
+  public double averageUsingForLoop(int[] nums) {
+    if (nums == null || nums.length == 0) { return 0; }
+    int sum = 0;
+      for (int num : nums) {
+          sum += num;
+      }
+      return (double) sum / nums.length;
+  }
+
+  public int maximumUsingStream(int[] nums) {
+  if (nums == null || nums.length == 0) { return 0; }
+  return Arrays.stream(nums).max().getAsInt();
+  }
+  public int minimumUsingStream(int[] nums) {
+  if (nums == null || nums.length == 0) { return 0; }
+  return Arrays.stream(nums).min().getAsInt();
+  }
+  public int sumUsingStream(int[] nums) {
+  if (nums == null || nums.length == 0) { return 0; }
+  return Arrays.stream(nums).sum();
+  }
+  public double averageUsingStream(int[] nums) {
+  if (nums == null || nums.length == 0) { return 0; }
+  return Arrays.stream(nums).average().getAsDouble();
+  }
   public static void main(String[] args) {
     System.out.println("This class can house your driver program and / or your functions.");
   }
--- src/test/java/com/csc/TestCalculator.java
+++ src/test/java/com/csc/TestCalculator.java
@@ -16,6 +16,24 @@ public class TestCalculator {
 
   @Test
   void itWorks() {
-    assertEquals(true, true);
+    int[] nums = {24,72,83,19,9};
+    int[] num = {1};
+    assertEquals(calculator.maximumUsingForLoop(nums), 83);
+    assertEquals(calculator.maximumUsingStream(nums), 83);
+    assertEquals(calculator.minimumUsingForLoop(nums), 9);
+    assertEquals(calculator.minimumUsingStream(nums), 9);
+    assertEquals(calculator.averageUsingForLoop(nums), 41.4);
+    assertEquals(calculator.averageUsingStream(nums), 41.4);
+    assertEquals(calculator.sumUsingForLoop(nums), 207);
+    assertEquals(calculator.sumUsingStream(nums), 207);
+    assertEquals(calculator.maximumUsingStream(num), 1);
+    assertEquals(calculator.maximumUsingForLoop(num), 1);
+    assertEquals(calculator.minimumUsingStream(num), 1);
+    assertEquals(calculator.minimumUsingForLoop(num), 1);
+    assertEquals(calculator.averageUsingForLoop(num), 1);
+    assertEquals(calculator.averageUsingStream(num), 1);
+    assertEquals(calculator.sumUsingStream(num), 1);
+    assertEquals(calculator.sumUsingForLoop(num), 1);
+
   }
 }
